af18d199a4b9a53684618140b18083592fc1fd56
lotos <lotositsh@gmail.com>
2024-09-23 15:31:31 +0300

    add medit

--- it/temp/css/scriptsearchbox.js
+++
@@ -1,9 +0,0 @@
-  document.getElementById('searchLink').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default behavior of the link
-    var searchBox = document.getElementById('searchBox');
-    if (searchBox.style.display === 'none' || searchBox.style.display === '') {
-      searchBox.style.display = 'block';
-    } else {
-      searchBox.style.display = 'none';
-    }
-  });
